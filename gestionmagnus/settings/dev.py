7f555abf75ef33181fe8c53979bb29ca934a6f4d
Ivan Navarro <sanotaro81@gmail.com>
2019-09-06 18:19:00 -0400

    first commit

---
+++ gestionmagnus/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8%v6$b2h(+$cq^&!n2q9qg#d3+_@e7$7v4a+erdfu7gk1u#n+d'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
